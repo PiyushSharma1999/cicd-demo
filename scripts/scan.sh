681a55e468358f7156ca856a2e8892f8f307c4d1
Piyush Sharma <pshar169@asu.edu>
2026-05-06 09:36:59 -0700

    trivy scan

---
+++ scripts/scan.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -euo pipefail
+MODE=$1 # "dockerfile" or "image"
+TARGET=$2
+
+echo "[trivy] Scanning $MODE: $TARGET"
+
+if [[ "$MODE" == "dockerfile" ]]; then
+    trivy config "$TARGET" --exit-code 1
+elif [[ "$MODE" == "image" ]]; then
+    trivy image \
+        --exit-code 1 \
+        --severity HIGH, CRITICAL \
+        --format sarif \
+        --output wiz-results.sarif
+        "$TARGET"
+else
+    echo "Unknown mode"; exit 1
+fi
+
+echo "[trivy] Scan complete"
+
+# echo "[wiz] Scanning $MODE: $TARGET"
+
+# if [[ "$MODE" == "dockerfile" ]]; then
+#     wizcli iac scan --path "$TARGET" --policy "dockerfile-baseline"
+# elif [[ "$MODE" == "image" ]]; then
+#     wizcli docker scan --image "$TARGET" \
+#         --policy "container-critical-and-high" \
+#         --output sarif > wiz-results.sarif
+# else
+#     echo "Unknown mode"; exit 1
+# fi
+
+# echo "[wiz] Scan complete"
